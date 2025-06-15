93cb7dc87e876ebf897a4fe9b0c4ed6de409b682
callum-vdm-dev <cvandermeulen.dev@gmail.com>
2025-06-15 15:59:32 +1000

    installed html5up template for home page.

---
+++ templates/layout/public.php
@@ -0,0 +1,27 @@
+<!DOCTYPE HTML>
+<!--
+	Photon by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+<head>
+    <title>Photon by HTML5 UP</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+    <link rel="stylesheet" href="public-facing/css/main.css" />
+    <noscript><link rel="stylesheet" href="public-facing/css/noscript.css" /></noscript>
+</head>
+<body class="is-preload">
+<?= $this->fetch('content') ?>
+
+<!-- Scripts -->
+<script src="public-facing/js/jquery.min.js"></script>
+<script src="public-facing/js/jquery.scrolly.min.js"></script>
+<script src="public-facing/js/browser.min.js"></script>
+<script src="public-facing/js/breakpoints.min.js"></script>
+<script src="public-facing/js/util.js"></script>
+<script src="public-facing/js/main.js"></script>
+
+</body>
+</html>
