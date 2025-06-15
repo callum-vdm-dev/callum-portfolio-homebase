0693cdef1c3fa08cc4325dbdc3250dea398b31e3
callum-vdm-dev <cvandermeulen.dev@gmail.com>
2025-06-15 16:59:57 +1000

    added navbar to the public layout

--- templates/layout/public.php
+++ templates/layout/public.php
@@ -13,6 +13,32 @@
     <noscript><link rel="stylesheet" href="public-facing/css/noscript.css" /></noscript>
 </head>
 <body class="is-preload">
+<!-- Nav -->
+<nav id="nav" class="custom-navbar">
+    <ul>
+        <li>
+            <a href="#">Dropdown</a>
+            <ul>
+                <li><a href="#">Lorem ipsum dolor</a></li>
+                <li><a href="#">Magna phasellus</a></li>
+                <li><a href="#">Etiam dolore nisl</a></li>
+                <li>
+                    <a href="#">Phasellus consequat</a>
+                    <ul>
+                        <li><a href="#">Lorem ipsum dolor</a></li>
+                        <li><a href="#">Phasellus consequat</a></li>
+                        <li><a href="#">Magna phasellus</a></li>
+                        <li><a href="#">Etiam dolore nisl</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">Veroeros feugiat</a></li>
+            </ul>
+        </li>
+        <li><a href="left-sidebar.html">Left Sidebar</a></li>
+        <li class="break"><a href="right-sidebar.html">Right Sidebar</a></li>
+        <li><a href="no-sidebar.html">No Sidebar</a></li>
+    </ul>
+</nav>
 <?= $this->fetch('content') ?>
 
 <!-- Scripts -->
